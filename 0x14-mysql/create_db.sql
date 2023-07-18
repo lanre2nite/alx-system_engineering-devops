e555e40720270a06d13c262cc81b1ed579de10a1
lanre2nite <olabagy@hotmail.com>
2023-07-18 13:25:03 +0100

    updating task files

---
+++ 0x14-mysql/create_db.sql
@@ -0,0 +1,8 @@
+-- Create database named tyrell_corp
+-- Create table nexus6 containing at least one entry
+CREATE DATABASE 
+IF NOT EXITS 'tyrell_corp';
+CREATE TABLE IF NOT EXITS 'tyrell_corp'.'nexus6';
+INSERT INTO TABLE 'nexus6' ('id', 'name') 
+VALUES (1, 'Leon');
+
---
+++ 0x14-mysql/create_user.sql
@@ -0,0 +1,6 @@
+-- Create user holberton and give access to primary/replica status of the database
+CREATE USER IF NOT EXITS 'holberton_user'@'localhost' 
+IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT SELECT, INSERT, UPDATE,REPLICATION CLIENT, REPLICATION SLAVE 
+ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
